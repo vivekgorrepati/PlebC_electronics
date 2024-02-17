4687ade2bed5c2854af5ce6109a3bbf6bfb101c4
hritikkumar0001 <hritikkumar0001@gmail.com>
2024-02-18 00:46:56 +0530

    Create list testing.py

---
+++ HRITIK/moon motors/list testing.py
@@ -0,0 +1,58 @@
+import time
+import minimalmodbus
+from datetime import datetime
+import random
+
+# Create a Modbus RTU motor1
+motor1 = minimalmodbus.Instrument('COM15', 1)  # Replace with your device's serial port and slave address
+# Set the communication parameters for motor1 (baudrate, parity, etc.)
+motor1.serial.baudrate = 9600
+motor1.serial.bytesize = 8
+motor1.serial.parity = minimalmodbus.serial.PARITY_NONE
+motor1.serial.stopbits = 1
+motor1.serial.timeout = 0.1 
+motor1.serial.xonxoff = False
+motor1.serial.rtscts = False
+motor1.serial.dsrdtr = False
+motor1.serial.writeTimeout = 0
+
+current_steps = 0
+
+# List of 500 angles between -360 and 360
+# angles_list = [i for i in range(-360, 361)]
+# angles_list = [45,90,135,180,225,270,315,360,]
+
+# Generate a list of 500 random integers between -360 and 360
+angles_list = [random.randint(-360, 360) for _ in range(10)]  # Generate 499 random angles
+
+# Add 0 as the last value in the list
+angles_list.append(0)
+
+for serial_number, angle in enumerate(angles_list, start=1):
+    try:
+        print(serial_number, angle)
+        target_steps = int((20000/360) * angle)  # to move forward write positive steps, for backward write negative steps
+        steps = target_steps - current_steps
+        values_to_write = [(steps >> 16) & 0xFFFF, steps & 0xFFFF]  # Split the 32-bit integer into two 16-bit values
+
+        registers_values = motor1.read_registers(4, 2)  # Read the values from two consecutive registers (registers 5 and 6) Encoder value
+        msbs = registers_values[0]  # Extract the Most Significant Bits (MSBs) from the long integer
+        encoder_value = msbs << 16 | registers_values[1]  # Combine the MSBs to form the complete 32-bit integer
+        print("Encoder value :", encoder_value, " i.e. time:", datetime.now().strftime("%H:%M:%S.%f")[:-3])
+
+        motor1.write_registers(30, values_to_write)  # Write the values to the registers
+        motor1.write_register(124, 102, functioncode=6)  # Send command to control register
+        current_steps = current_steps + steps  # Updating steps
+        
+        registers_values = motor1.read_registers(4, 2)  # Read the values from two consecutive registers (registers 5 and 6) Encoder value
+        msbs = registers_values[0]  # Extract the Most Significant Bits (MSBs) from the long integer
+        encoder_value = msbs << 16 | registers_values[1]  # Combine the MSBs to form the complete 32-bit integer
+        print("Encoder value :", encoder_value, " time:", datetime.now().strftime("%H:%M:%S.%f")[:-3])
+        time.sleep(0.06)
+
+    except Exception as e:
+        print(f"Error writing to holding registers: {str(e)}")
+
+    finally:
+        # Close the communication port (assuming you want to close it after each iteration)
+        motor1.serial.close()
